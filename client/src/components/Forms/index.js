1ae117a754384fa77e21a0c7be04e24c5537eb8d
Sauby <imsauby@gmail.com>
2024-02-13 23:26:13 +0530

    created different forms

--- client/src/components/Forms/index.js
+++ client/src/components/Forms/index.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Stack, TextField, Button } from "@mui/material";
+import {
+  Stack,
+  TextField,
+  Button,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
 
 export const Overview = ({ handleStepForward }) => {
   return (
@@ -21,17 +30,91 @@ export const Overview = ({ handleStepForward }) => {
   );
 };
 
-export const UserInfo = ({ handleStep }) => {
+export const UserInfo = ({ handleStepForward, handleStepBackward }) => {
   return (
     <>
-      <form>
-        <TextField id="outlined-basic" label="First name" variant="outlined" />
-        <TextField id="outlined-basic" label="Last name" variant="outlined" />
+      <h2>First, may I know your name?</h2>
+      <form className="basic-form">
+        <TextField
+          id="outlined-basic"
+          label="First name"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="outlined-basic"
+          label="Last name"
+          variant="outlined"
+          fullWidth
+        />
+
       </form>
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <Button variant="outlined" onClick={() => handleStepBackward()}>
+          Back
+        </Button>
+        <Button variant="outlined" onClick={() => handleStepForward()}>
+          Next
+        </Button>
+      </Stack>
     </>
   );
 };
 
-export const VehicleSelect = ({ handleStep }) => {};
+export const VehicleSelect = ({ handleStepForward, handleStepBackward }) => {
+  return (
+    <>
+      <h2>Choose your poison...</h2>
+      <FormControl>
+        <FormLabel id="demo-row-radio-buttons-group-label">
+          Two or Four Wheeler
+        </FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="demo-row-radio-buttons-group-label"
+          name="row-radio-buttons-group"
+        >
+          <FormControlLabel
+            value="female"
+            control={<Radio />}
+            label="Two wheeler"
+          />
+          <FormControlLabel
+            value="male"
+            control={<Radio />}
+            label="Four wheeler"
+          />
+        </RadioGroup>
+      </FormControl>
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <Button variant="outlined" onClick={() => handleStepBackward()}>
+          Back
+        </Button>
+        <Button variant="outlined" onClick={() => handleStepForward()}>
+          Next
+        </Button>
+      </Stack>
+    </>
+  );
+};
 
-export const DatePick = ({ handleStep }) => {};
+export const DatePick = ({ handleStepBackward }) => {
+  return (
+    <>
+      <h2>On which date?</h2>
+      <form className="basic-form">
+        <input type="date" />
+
+      </form>
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <Button variant="outlined" onClick={() => handleStepBackward()}>
+          Back
+        </Button>
+
+        <Button variant="contained" onClick={() => handleStepBackward()}>
+          Book now!
+        </Button>
+      </Stack>
+    </>
+  );
+};
