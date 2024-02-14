3addd76d13b75eba286bbc535674082e0a217448
Sauby <imsauby@gmail.com>
2024-02-14 12:27:55 +0530

    added endpoints for the vehicles

--- server/index.js
+++ server/index.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-const sqlite3 = require("sqlite3").verbose();
-const schema = require("./schema");
+const db = require("./schema");
 
 const PORT = 9872;
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  db.all("SELECT * FROM vehicles", (err, rows) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Internal Server Error");
+    } else return res.status(200).send(rows);
+  });
 });
 
-db.close();
+app.get("/wheels", (req, res) => {
+  const wheels = req.query.wheels;
+
+  db.all(`SELECT * FROM vehicles where wheels=${wheels}`, (err, rows) => {
+    if (err) {
+      console.log("Internal Server Error!");
+      return res.status(500).send("Internal Server Error");
+    } else return res.status(200).send(rows);
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Running on http://localhost:" + PORT);
--- server/schema.js
+++ server/schema.js
@@ -5,45 +5,57 @@ const db = new sqlite3.Database("./db/main.db", (err) => {
   else console.log("Connected to the vehicles database.");
 });
 
-
 // created schema
 db.serialize(() => {
   db.run(`
-          CREATE TABLE vehicles (
+          CREATE TABLE IF NOT EXISTS vehicles (
               id INTEGER PRIMARY KEY,
               vehicleName TEXT,
               wheels INTEGER,
               vehicleType TEXT,
+              isBooked INTEGER
           )
       `);
 
-  db.rub(`
-          CREATE TABLE user (
-              id INTEGER PRIMARY KEY,
-              firstname TEXT,
-              lastname TEXT,
-              wheels INTEGER,
-              vehicleType TEXT,
-              startDate TIMESTAMP,
-              endDate TIMESTAMP,
-          )
-      `);
+  // db.run(`
+  //         CREATE TABLE IF NOT EXISTS user (
+  //             id INTEGER PRIMARY KEY,
+  //             firstname TEXT,
+  //             lastname TEXT,
+  //             wheels INTEGER,
+  //             vehicleType TEXT,
+  //             startDate TIMESTAMP,
+  //             endDate TIMESTAMP
+  //         )
+  //     `);
 });
 
+// added some
 db.serialize(() => {
-  db.run(
-    `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Land Rover Defender", 4, "Hatchback")`
-  );
-  db.run(
-    `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Porsche 911", 4, "SUV")`
-  );
-  db.run(
-    `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Volkswagen Virtus", 4, "Sedan")`
-  );
-  db.run(
-    `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Harley-Davidson Iron 883", 2, "Cruiser")`
-  );
-  db.run(
-    `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Yamaha YZF-R1", 2, "Sports")`
-  );
+  db.get("SELECT COUNT(*) AS count FROM vehicles", (err, row) => {
+    if (err) {
+      console.error("Error checking for existing records:", err.message);
+      return;
+    }
+
+    if (row.count === 0) {
+      db.run(
+        `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Land Rover Defender", 4, "Hatchback")`
+      );
+      db.run(
+        `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Porsche 911", 4, "SUV")`
+      );
+      db.run(
+        `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Volkswagen Virtus", 4, "Sedan")`
+      );
+      db.run(
+        `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Harley-Davidson Iron 883", 2, "Cruiser")`
+      );
+      db.run(
+        `INSERT INTO vehicles (vehicleName, wheels, vehicleType) VALUES ("Yamaha YZF-R1", 2, "Sports")`
+      );
+    } else console.log("Vehicles already contains data. Skipping adding data.");
+  });
 });
+
+module.exports = db;
